b78d9dfded6cc4bffd8017b23d516912c9d6a708
hedibousbih <Mohamedhedi.Bousbih@esprit.tn>
2024-04-21 16:24:24 +0100

    Affichage angular

--- src/app/Blog/list-blogback/list-blogback.component.ts
+++ src/app/Blog/list-blogback/list-blogback.component.ts
@@ -19,58 +19,17 @@ export class ListBlogbackComponent implements OnInit {
   ngOnInit(): void {
     this.fetchPublications();
   }
-
   fetchPublications(): void {
     this.blogService.getBlogList()
       .subscribe({
         next: (publications) => {
           this.publications = publications;
-          this.filteredPublications = this.publications;
-          this.sortPublications();
+          console.log('Fetched publications:', this.publications); // Log the fetched publications
         },
         error: (error) => {
           console.error(error);
         }
       });
-  }
-
-  sortPublications(): void {
-    this.publications.sort((a, b) => {
-      if (a[this.sortBy] < b[this.sortBy]) {
-        return 1;
-      } else if (a[this.sortBy] > b[this.sortBy]) {
-        return -1;
-      } else {
-        return 0;
-      }
-    });
-  }
-
-  onSortChange(): void {
-    this.sortPublications();
-  }
-
-  deletePublication(publication: Publication): void {
-    this.blogService.deleteBlog(publication.id).subscribe(() => {
-      this.publications = this.publications.filter((p) => p.id !== publication.id);
-      this.filteredPublications = this.publications; // Update filtered list after deletion
-    });
-  }
-
-  navigateToUpdate(publicationId: string): void {
-    this.router.navigate(['/updatePublication/', publicationId]);
-  }
-
-  navigateToAddPublication() {
-    this.router.navigate(['/addPublication']);
-  }
-
-  onSearch(): void {
-    this.filteredPublications = this.publications.filter(publication =>
-      (publication.Sujet && publication.Sujet.toLowerCase().includes(this.searchInput.toLowerCase())) ||
-      (publication.contenu && publication.contenu.toLowerCase().includes(this.searchInput.toLowerCase())) ||
-      (publication.dateCreation && publication.dateCreation.toString().toLowerCase().includes(this.searchInput.toLowerCase()))
-    );
   }
 }
 
--- src/app/Models/Blog/publication.ts
+++ src/app/Models/Blog/publication.ts
@@ -1,8 +1,8 @@
 import { User } from "../user/user";
 
 export class Publication {
-    id?: any;
-    Sujet?: string;
+    numPub?: any;
+    sujet?: string;
     contenu?: string;
     dateCreation?: Date;
     photo?: string;
